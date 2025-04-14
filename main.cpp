2dae43240478241bdb1b31ccaa9f40fe11b2e567
1b31d107 <1b31d107@gmail.com>
2025-04-14 11:19:46 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,72 +1,24 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-class User {
-	string username, password;
-public:
-	User() {}
-	User(string username, string password) {
-		this->username = username;
-		this->password = password;
-		users.push_back({username, password});
-	}
-
-	bool operator==(User user) { return (username == user.username); }
-
-	void display() {
-		cout << "\nUser info: " << endl;
-		cout << "Username: " << username << endl;
-		cout << "Password: " << password << "\n\n";
-	}
-	static vector<vector<string>> users;
-	
-	bool is_existing() {
-		for (int i = 0; i < users.size(); i++) {
-			cout << users[i][0] << ":" << users[i][1] << endl;
-			if (users[i][0] == username) return 1;
-		}
-		return 0;
-	}
+class Balance {
+	// balance, transaction, fill
 };
 
-class Product {
+class Customer {
 private:
-	string name;
-	float price;
-	string description;
-public:
-	Product(string name, float price, string description) {
-		this->name = name;
-		this->price = price;
-		this->description = description;
-	}
-	void display() {
-		cout << "\nProduct Info: " << endl;
-		cout << "Name: " << name << endl;
-		cout << "Price: " << price << endl;
-		cout << "Description: " << description << endl;
-	}
+	// view, product, buy, history, wishlist, cart, payment (refund)
 };
 
-class Customer : public User {
-public:
-	void buy_product(Product product) {
-
-	}
+class Seller {
+	// add, remove, update, show product announcements
+	// discount, profit
 };
 
-/*
-
-class File {};
-
-*/
-
-vector<vector<string>> User::users = {};
+class User : public Seller, public Customer {
+	// login, register, logout, change password
+};
 
 int main() {
-	User u1("name", "passwd");
-	User u2("h4x0r", "uwillneverguess7h1s");
-	User u3("bro", "whatdahell");
-	cout << u3.is_existing();
+
 }
