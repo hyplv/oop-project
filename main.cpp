4f22b2e75866ad7cfd227c97a1655d28577a5f43
1b31d107 <1b31d107@gmail.com>
2025-04-27 01:07:08 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <string>
+#include <sstream>
 using namespace std;
 
 
@@ -10,14 +13,15 @@ private:
 
 	string id;
 	string name;
-	double price;
 	int quantity;
+	double price;
 	double discount;
 
 public:
-	Product() { cout << "whatever" << endl; };
 
-	Product(string id, string name, double price, int quantity = 0, double discount = 0.0) {
+	// Product() { cout << "whatever" << endl; };
+
+	Product(string id, string name, int quantity = 0, double price = 0.0, double discount = 0.0) {
 		this->id = id;
 		this->name = name;
 		this->price = price;
@@ -28,21 +32,35 @@ public:
 	// getters
 	string get_id() const { return id; }
 	string get_name() const { return name; }
-	double get_price() const { return price; }
 	int get_quantity() const { return quantity; }
+	double get_price() const { return price; }
+	double get_discount() const { return discount; };
 	double get_discounted_price() const { return (price - price * discount / 100); }
 	
 	// setters
-	void set_quantity(int amount) { quantity = amount; };
-	void set_discount(double percentage) { discount = percentage; };
+	void set_quantity(int amount) { quantity = amount; }
+	void set_price(double amount) { price = amount; }
+	void set_discount(double percentage) {
+		if (percentage > 0 and percentage < 100)
+			discount = percentage;
+		else
+			cout << "Discount percentage should be between 0-100!" << endl;
+	}
 
-	bool operator==(const Product& other) const { return id == other.id; };
+	bool operator==(const Product& other) const { return id == other.id; }
 	
 	Product operator+(int quantity) {
-		Product result = *this;
-		result.quantity += quantity;
-		return result;
-	};
+		this->quantity += quantity;
+		return *this;
+	}
+	friend ostream& operator<<(ostream& out, Product obj) {
+		cout << "ID: " << obj.id << endl;
+		cout << "Name: " << obj.name << endl;
+		cout << "Price: " << obj.price << endl;
+		cout << "Discount: " << obj.discount << endl;
+		cout << "Discounted Price: " << obj.get_discounted_price() << endl;
+		return out;
+	}
 };
 
 // ======================================================================================= //
@@ -67,8 +85,13 @@ public:
 		if (position != items.size()) items.erase(position);
 	};
 	*/
+
 	double get_total() const {
-		return 
+		double result = 0.0;
+		for (int i = 0; i < items.size(); i++) {
+			result += items[i].get_discounted_price() * items[i].get_quantity();
+		}
+		return result;
 	};
 	
 	void display_cart() const {
@@ -88,32 +111,52 @@ public:
 
 
 // ======================================================================================= //
+
 class Transaction {
 private:
 
 	string id;
 	string customer_username;
 	vector<Product> purchased_items;
-	double total_amount;
 	string timestamp;
 
 public:
 
-	Transaction(string id, string customer_username, vector<Product> purchased_items, double total_amount, string timestamp) {
+	Transaction(string id, string customer_username, vector<Product> purchased_items, string timestamp) {
 		this->id = id;
 		this->customer_username = customer_username;
 		this->purchased_items = purchased_items;
-		this->total_amount = total_amount; // mayber use get_total or smth
 		this->timestamp = timestamp; // should replace with the current date using some library or smth
 	}
 
-	void generate_receipt() const {
-		cout << "" << endl; // print out all the data prettily
+	// getters
+	string get_id() const { return id; }
+	string get_customer_username() const { return customer_username; }
+	vector<Product> get_purchased_items() const { return purchased_items; }
+	double total_amount() const {
+		double result = 0;
+		for (int i = 0; i < purchased_items.size(); i++) {
+			result += purchased_items[i].get_discounted_price() * purchased_items[i].get_quantity();
+		}
+		return result;
 	}
+	string get_timestamp() const { return timestamp; }
 
-	string get_time_stamp() const { return timestamp; }
+
+	void generate_receipt() const {
+		cout << "ID: " << id << endl;
+		cout << "Customer username: " << customer_username << endl;
+		cout << "Purchased items:" << endl;
+		for (int i = 0; i < purchased_items.size(); i++) {
+			cout << "\t\t" << purchased_items[i] << endl;
+		}
+		cout << "Total Amount: " << total_amount() << endl;
+		cout << "Timestamp: " << endl;
+	}
 };
+
 // ======================================================================================= //
+
 class Balance {
 private:
 
@@ -135,7 +178,7 @@ public:
 	double get_amount() const { return amount; };
 	
 	void display() const {
-		cout << "" << endl;
+		cout << "Balance: " << amount << endl;
 	};
 };
 
@@ -143,30 +186,60 @@ public:
 
 class User {
 protected:
-
 	string username;
 	string password;
 
 public:
-
+	User() {}
+	User(string u, string p) : username(u), password(p) {
+		login();
+	}
+	
+	virtual string get_username() { return username; }
+	virtual string get_password() { return password; }
+	
 	virtual void display_menu() = 0;
 
+	virtual void signin() {
+		cout << "\n\t\t\t\tRegistration" << endl;
+		string usrnm, passwd1, passwd2;
+		
+		cout << "Enter username:";
+		cin >> usrnm;
+		cout << "Enter password:";
+		cin >> passwd1;
+		cout << "Enter password again:";
+		cin >> passwd2;
+		
+		if (passwd1 == passwd2) {
+			username = usrnm;
+			password = passwd1;
+			cout << "Registration successful!" << endl;
+		}
+		else {
+			cout << "Password did not match!" << endl;
+		}
+	}
+
 	virtual void login() {
+		cout << "\n\t\t\t\tLogging in" << endl;
 		string tmp_usrname, tmp_passwd;
+
 		cout << "Enter username:";
 		cin >> tmp_usrname;
 		cout << "Enter password:";
 		cin >> tmp_passwd;
-		if (username == tmp_usrname and password == "tmp_passwd")
+		cout << tmp_usrname << " " << tmp_passwd << endl;
+		
+		if (username == tmp_usrname && password == tmp_passwd)
 			display_menu();
 		else
-			cout << "Either username or password is wrong" << endl;
+			cout << "Either username or password is wrong. Try again!" << endl;
 	};
 
 	virtual void logout() {
 		cout << "Loggin out... " << endl;
 	};
-	// virtual ~User();
 };
 
 // ======================================================================================= //
@@ -175,40 +248,45 @@ class Customer : virtual public User {
 private:
 	Balance wallet;
 	Cart cart;
+
 public:
-	void display_menu() override;
-	void add_to_cart(const Product& p) { cart.add_product(p); };
-	void checkout();
-	void top_up_balance(double amount);
-	void view_order_history();
-	void view_balance() const;
+	Customer() {}
+	Customer(string u, string p) : User(u, p) {}
+
+	void display_menu() override {
+		cout << "\t\t\tUSER PANEL" << endl;
+		cout << "[1] view products" << endl;
+		cout << "[2] view history" << endl;
+		cout << "[3] view balance" << endl;
+	}
+
+	void add_to_cart(const Product& p) { cart.add_product(p); }
+	
+	void checkout() {
+		wallet.withdraw(cart.get_total());
+		cart.clear();
+	}
+	
+	void top_up_balance(double amount) { wallet.deposit(amount); }
+	
+	void view_order_history() {}
+	
+	void view_balance() const { wallet.display(); }
 };
 
 // ======================================================================================= //
 
-class Seller : virtual public User {
-private:
-	Balance earnings;
-public:
-	void display_menu() override;
-	void process_sale();
-	void view_sales_history();
-	void apply_discount(Product& p, double amount);
-	void view_earnings() const;
-};
-
-// ======================================================================================= //
-
-class CustomerSeller : public Customer, public Seller {
-public:
-	void display_menu() override;
-};
-
-// ======================================================================================= //
-
-class Admin : User {
-	void display_menu() override;
-	void add_product(Product& p);
+class Admin : public User {
+	void display_menu() override {
+		cout << "\t\t\t\tADMIN PANEL:\t\t" << endl;
+		cout << "[1] manage products" << endl;
+		cout << "[2] manage users" << endl;
+		cout << "[3] view inventory" << endl;
+		cout << "[4] view sales report" << endl;
+	}
+	void add_product(Product& p) {
+		
+	}
 	void edit_product(Product& p);
 	void delete_product(Product& p);
 	void manage_users();
@@ -219,21 +297,121 @@ class Admin : User {
 // ======================================================================================= //
 
 class FileHandler {
-public:
-	static vector<User*> load_users(const string& filename);
-	static void save_users(const vector<User*>& users, const string& filename);
-	
-	static vector<Product> load_products(const string& filename);
-	static void save_products(const vector<Product>& products, const string& filename);
-	
-	static void log_transactions(const Transaction& t, const string& filename);
-	static vector<Transaction> load_transactions(const string& filename);
+private:
+	static vector<string> parse(string mess, char delimiter) {
+		vector<string> parsed;
+		stringstream ss(mess);
+		string line;
+		
+		while (getline(ss, line, delimiter)) {
+			parsed.push_back(line);
+		}
+		return parsed;
+	}
 
-	static void generate_receipt(const Transaction& t);
-	static bool file_exists(const string& filename);
+public:
+
+	static void save_users(const vector<User*>& users, const string& filename) {
+		fstream file;
+		file.open(filename, ios::out, ios::trunc);
+		for (int i = 0; i < users.size(); i++) {
+			file << users[i]->get_username() << ":" << users[i]->get_password() << "\n";
+		}
+	}
+
+	static vector<User*> load_users(const string& filename) {
+		vector<User*> result;
+		fstream file;
+		file.open(filename, ios::in);
+		
+		if (!file.is_open()) {
+			cerr << "Failed to open " << filename << endl;
+			return {};
+		}
+		
+		string line;
+		while (getline(file, line)) {
+			vector<string> parsed = parse(line, ':');
+			result.push_back(new Customer(parsed[0], parsed[1]));
+		}
+		return result;
+	}
+
+
+	static void save_products(const vector<Product>& products, const string& filename) {
+		fstream file;
+		file.open(filename, ios::out, ios::trunc);
+		file << "ID\t\tNAME\t\tQUANTITY\t\tPRICE\t\tDISCOUNT" << endl;
+		for (int i = 0; i < products.size(); i++) {
+			file << products[i].get_id() << ":"
+				 << products[i].get_name() << ":"
+				 << products[i].get_quantity() << ":"
+				 << products[i].get_price() << ":"
+				 << products[i].get_discount() << endl;
+		}
+	}
+	
+	static vector<Product> load_products(const string& filename) {
+		vector<Product> result;
+		fstream file(filename, ios::in);
+		string line;
+
+		if (!file.is_open()) {
+			cerr << "Error! Couldn't open " << filename << endl;
+			return {};
+		}
+		
+		while (getline(file, line)) {
+			vector<string> parsed = parse(line, ':');
+			if (parsed.size() == 5)
+				result.push_back(Product(parsed[0], parsed[1], stoi(parsed[2]), stoi(parsed[3]), stoi(parsed[4])));
+			else
+				cout << parsed.size() << "some kind of error occured while loading the products!" << endl;
+		}
+		return result;
+	}
+	// whatever will rewrite it later
+	static void log_transactions(const Transaction& t, const string& filename) {
+		fstream file(filename, ios::out, ios::trunc);
+		vector<Product> purchased = t.get_purchased_items();
+
+		if (!file.is_open()) {
+			cerr << "Error! Couldn't open " << filename << endl;
+			return;
+		}
+
+		file << t.get_id() << ":" << t.get_customer_username() << ":";
+		
+		for (int i = 0; i < purchased.size(); i++) {
+			file << purchased[i].get_name() << ","
+				 << purchased[i].get_price() << ","
+				 << purchased[i].get_quantity() << "|";
+		}
+
+		file << ":" << t.total_amount() << ":" << t.get_timestamp() << endl;
+	}
+	/*
+	static vector<Transaction> load_transactions(const string& filename) {
+		fstream file(filename, ios::in);
+		string line;
+		while (getline(file, line, ',')) {
+			
+		}
+	}
+	*/
+	/*
+	static void generate_receipt(const Transaction& t) {
+	}
+	*/
 };
 // ======================================================================================= //
-
+/*
+class Shop() {};
+*/
+//
 int main() {
-
+	Customer c1;
+	c1.signin();
+	c1.login();
+	return 0;
 }
